f1302e4f09f25dafe23cf349dade151ea5593838
Rameez Ahmed Khan <rameez.ahmed@nextgeni.com>
2024-01-08 11:24:05 +0500

    setup completed of note detail screen

---
+++ app/src/main/java/com/example/memomate/screens/detail_note/NoteDetailScreen.kt
@@ -0,0 +1,15 @@
+package com.example.memomate.screens.detail_note
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NoteDetailScreen() {
+
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun PreviewFunction() {
+
+}
