d7fe6bedba8ca1328775fe5a7d38082a69f549fc
Rameez Ahmed Khan <rameez.ahmed@nextgeni.com>
2024-01-08 11:05:14 +0500

    some fixes after merging

--- app/src/main/java/com/example/memomate/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/memomate/screens/home/HomeScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,23 +19,21 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.memomate.model.NotesModel
 import com.example.memomate.screens.add_note.CreateNoteScreen
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.memomate.model.NotesModel
 
 
 @Composable
@@ -94,18 +91,12 @@ fun HomeScreen(navController: NavController) {
                     }
                 })
 
-            FloatingActionButton(
-                shape = CircleShape,
-                onClick = {
-                    navController.navigate("CreateNoteScreen")
-                },
-                modifier = Modifier
-            Spacer(modifier = Modifier.align(Alignment.BottomEnd).width(10.dp).height(20.dp))
+//            Spacer(modifier = Modifier.align(Alignment.BottomEnd).width(10.dp).height(20.dp))
 
             FloatingActionButton(
                 shape = CircleShape,
                 onClick = {
-                    // Handle the FAB click (e.g., navigate to compose screen)
+                    navController.navigate("CreateNoteScreen")
                 },
                 modifier = Modifier
                     .offset(x = (-25).dp, y = (-40).dp)
